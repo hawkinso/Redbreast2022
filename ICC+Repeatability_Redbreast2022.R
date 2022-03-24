c301a3352083c45c3a2b403d39badfcda321598e
ohawkins1 <ohawkins.research@gmail.com>
2022-03-24 11:30:44 -0500

    adjust figure, update code to account for magnification data

--- ICC+Repeatability_Redbreast2022.R
+++ ICC+Repeatability_Redbreast2022.R
@@ -31,7 +31,7 @@ data <- read.csv("Redbreast2022_FINAL.csv")
 # Subset data that will be used in analysis ----
 # Individual
 all.data <- data %>%
-  dplyr::select(Individual,SL,PG,TTO,TTC,PPROT,PPROTVEL,tPPROT,VELPG,maxVEL,tmaxVEL,ACCPG,H_L_ratio,AI,ingested_volume,PPDiopen,timeatcapture,VELpreycapture)%>%
+  dplyr::select(Individual,SL_mag,PG_mag,TTO,TTC,PPROT_mag,PPROTVEL_mag,tPPROT,VELPG_mag,maxVEL_mag,tmaxVEL,ACCPG_mag,H_L_ratio,AI,ingested_volume_mag,PPDiopen_mag,timeatcapture,VELpreycapture_mag)%>%
   group_by(Individual)%>%
   convert_as_factor(Individual)
 
@@ -47,22 +47,22 @@ all.data <- data %>%
 # STEPS: 
     # ONE: Subset the data to where it only has the kinematic variable and Individuals
 
-pg<- data %>% select(Individual,PG)
+pg<- data %>% select(Individual,PG_mag)
 tto<- data %>% select(Individual,TTO)
 ttc<- data %>% select(Individual,TTC)
-pprot<- data %>% select(Individual,PPROT)
-pprotvel<- data %>% select(Individual,PPROTVEL)
+pprot<- data %>% select(Individual,PPROT_mag)
+pprotvel<- data %>% select(Individual,PPROTVEL_mag)
 tpprot <- data %>% select(Individual,tPPROT)
-velpg<- data %>% select(Individual,VELPG)
-maxvel<- data %>% select(Individual,maxVEL)
+velpg<- data %>% select(Individual,VELPG_mag)
+maxvel<- data %>% select(Individual,maxVEL_mag)
 tmaxvel<- data %>% select(Individual,tmaxVEL)
-accpg<- data %>% select(Individual,ACCPG)
+accpg<- data %>% select(Individual,ACCPG_mag)
 ratio<- data %>% select(Individual,H_L_ratio)
 ai<- data %>% select(Individual,AI)
-vol<- data %>% select(Individual,ingested_volume)
-ppd<- data %>% select(Individual,PPDiopen)
+vol<- data %>% select(Individual,ingested_volume_mag)
+ppd<- data %>% select(Individual,PPDiopen_mag)
 time<- data %>% select(Individual,timeatcapture)
-velpc<- data %>% select(Individual,VELpreycapture)
+velpc<- data %>% select(Individual,VELpreycapture_mag)
 
 
 # TWO: Run the ICC model 
@@ -72,48 +72,48 @@ velpc<- data %>% select(Individual,VELpreycapture)
 # alpha : significance cut off 
 # CI.type : THD for unbalanced design
 
-pg.icc <- ICCest(Individual, PG, data = pg, alpha = 0.05, CI.type = c("THD"))
-tto.icc <- ICCest(Individual, TTO, data = tto, alpha = 0.05, CI.type = c("THD"))
-ttc.icc <- ICCest(Individual, TTC, data = ttc, alpha = 0.05, CI.type = c("THD"))
-pprot.icc <- ICCest(Individual, PPROT, data = pprot, alpha = 0.05, CI.type = c("THD"))
-pprotvel.icc <- ICCest(Individual, PPROTVEL, data = pprotvel, alpha = 0.05, CI.type = c("THD"))
-tpprot.icc <- ICCest(Individual, tPPROT, data = tpprot, alpha = 0.05, CI.type = c("THD"))
-velpg.icc <- ICCest(Individual, VELPG, data = velpg, alpha = 0.05, CI.type = c("THD"))
-maxvel.icc <- ICCest(Individual, maxVEL, data = maxvel, alpha = 0.05, CI.type = c("THD"))
-tmaxvel.icc <- ICCest(Individual, tmaxVEL, data = tmaxvel, alpha = 0.05, CI.type = c("THD"))
-accpg.icc <- ICCest(Individual, ACCPG, data = accpg, alpha = 0.05, CI.type = c("THD"))
-ratio.icc <- ICCest(Individual, H_L_ratio, data = ratio, alpha = 0.05, CI.type = c("THD"))
-ai.icc <- ICCest(Individual, AI, data = ai, alpha = 0.05, CI.type = c("THD"))
-vol.icc <- ICCest(Individual, ingested_volume, data = vol, alpha = 0.05, CI.type = c("THD"))
-ppd.icc <- ICCest(Individual, PPDiopen, data = ppd, alpha = 0.05, CI.type = c("THD"))
-time.icc <- ICCest(Individual, timeatcapture, data = time, alpha = 0.05, CI.type = c("THD"))
-velpc.icc <- ICCest(Individual, VELpreycapture, data = velpc, alpha = 0.05, CI.type = c("THD"))
+pg.icc <- ICCest(Individual, PG_mag, data = pg, alpha = 0.05, CI.type = c("Smith"))
+tto.icc <- ICCest(Individual, TTO, data = tto, alpha = 0.05, CI.type = c("Smith"))
+ttc.icc <- ICCest(Individual, TTC, data = ttc, alpha = 0.05, CI.type = c("Smith"))
+pprot.icc <- ICCest(Individual, PPROT_mag, data = pprot, alpha = 0.05, CI.type = c("Smith"))
+pprotvel.icc <- ICCest(Individual, PPROTVEL_mag, data = pprotvel, alpha = 0.05, CI.type = c("Smith"))
+tpprot.icc <- ICCest(Individual, tPPROT, data = tpprot, alpha = 0.05, CI.type = c("Smith"))
+velpg.icc <- ICCest(Individual, VELPG_mag, data = velpg, alpha = 0.05, CI.type = c("Smith"))
+maxvel.icc <- ICCest(Individual, maxVEL_mag, data = maxvel, alpha = 0.05, CI.type = c("Smith"))
+tmaxvel.icc <- ICCest(Individual, tmaxVEL, data = tmaxvel, alpha = 0.05, CI.type = c("Smith"))
+accpg.icc <- ICCest(Individual, ACCPG_mag, data = accpg, alpha = 0.05, CI.type = c("Smith"))
+ratio.icc <- ICCest(Individual, H_L_ratio, data = ratio, alpha = 0.05, CI.type = c("Smith"))
+ai.icc <- ICCest(Individual, AI, data = ai, alpha = 0.05, CI.type = c("Smith"))
+vol.icc <- ICCest(Individual, ingested_volume_mag, data = vol, alpha = 0.05, CI.type = c("Smith"))
+ppd.icc <- ICCest(Individual, PPDiopen_mag, data = ppd, alpha = 0.05, CI.type = c("Smith"))
+time.icc <- ICCest(Individual, timeatcapture, data = time, alpha = 0.05, CI.type = c("Smith"))
+velpc.icc <- ICCest(Individual, VELpreycapture_mag, data = velpc, alpha = 0.05, CI.type = c("Smith"))
 
 
 
 # THREE: Export ICC output to DataFrame
 
 ICCResults <- function(Grouping, var){
-  mod <- ICCest(Grouping, var, alpha = 0.05, CI.type = c("THD"))
+  mod <- ICCest(Grouping, var, alpha = 0.05, CI.type = c("Smith"))
   data.frame(mod$ICC, mod$LowerCI, mod$UpperCI, mod$N, mod$k, mod$varw, mod$vara)
 }
 
-pg.icc <- ICCResults(pg$Individual, pg$PG)
+pg.icc <- ICCResults(pg$Individual, pg$PG_mag)
 tto.icc <- ICCResults(tto$Individual, tto$TTO)
 ttc.icc <- ICCResults(ttc$Individual, ttc$TTC)
-pprot.icc <- ICCResults(pprot$Individual, pprot$PPROT)
-pprotvel.icc <- ICCResults(pprotvel$Individual, pprotvel$PPROTVEL)
+pprot.icc <- ICCResults(pprot$Individual, pprot$PPROT_mag)
+pprotvel.icc <- ICCResults(pprotvel$Individual, pprotvel$PPROTVEL_mag)
 tpprot.icc <- ICCResults(tpprot$Individual, tpprot$tPPROT)
-velpg.icc <- ICCResults(velpg$Individual, velpg$VELPG)
-maxvel.icc <- ICCResults(maxvel$Individual, maxvel$maxVEL)
+velpg.icc <- ICCResults(velpg$Individual, velpg$VELPG_mag)
+maxvel.icc <- ICCResults(maxvel$Individual, maxvel$maxVEL_mag)
 tmaxvel.icc <- ICCResults(tmaxvel$Individual, tmaxvel$tmaxVEL)
-accpg.icc <- ICCResults(accpg$Individual, accpg$ACCPG)
+accpg.icc <- ICCResults(accpg$Individual, accpg$ACCPG_mag)
 ratio.icc <- ICCResults(ratio$Individual, ratio$H_L_ratio)
 ai.icc <- ICCResults(ai$Individual, ai$AI)
-vol.icc <- ICCResults(vol$Individual, vol$ingested_volume)
-ppd.icc <- ICCResults(ppd$Individual, ppd$PPDiopen)
+vol.icc <- ICCResults(vol$Individual, vol$ingested_volume_mag)
+ppd.icc <- ICCResults(ppd$Individual, ppd$PPDiopen_mag)
 time.icc <- ICCResults(time$Individual, time$timeatcapture)
-velpc.icc <- ICCResults(velpc$Individual, velpc$VELpreycapture)
+velpc.icc <- ICCResults(velpc$Individual, velpc$VELpreycapture_mag)
 
 
 #Creating DataFrames 
@@ -147,10 +147,11 @@ var_break <- c("PG", "TTO", "TTC", "PPROT", "PPROTVEL", "tPPROT", "VELPG",
                "maxVEL", "tmaxVEL", "ACCPG", "timeatcapture", "H:L", 
                "AI", "ingested_volume", "PPDiopen", "VelPreyCapture")
 
+ICC_all <- ICC_all[1:14,]
 
 ggplot(ICC_all, aes(x=variable, y=mod.ICC, color=Category))+
   geom_point(aes(size=2))+
-  scale_color_brewer(palette = "Dark2")+
+  scale_color_manual(values=park_palettes$Everglades)+
   geom_errorbar(aes(ymin=mod.LowerCI, ymax=mod.UpperCI), width=0.5, size=1)+
   geom_hline(yintercept = 0.7, linetype=2)+
   facet_grid(~Category, scales="free_x") +
